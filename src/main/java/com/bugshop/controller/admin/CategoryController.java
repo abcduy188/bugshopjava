38950500eb3bd9b67319110e5dd2214b7ee25976
abcduy <tr.khanhsduy@gmail.com>
2022-03-05 21:19:38 +0700

    redirect

--- src/main/java/com/bugshop/controller/admin/CategoryController.java
+++ src/main/java/com/bugshop/controller/admin/CategoryController.java
@@ -1,5 +1,7 @@
 package com.bugshop.controller.admin;
 
+import java.lang.ProcessBuilder.Redirect;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.bugshop.dto.CategoryDTO;
 import com.bugshop.service.ICategoryService;
@@ -20,27 +23,29 @@ public class CategoryController {
 
 	@Autowired
 	private ICategoryService iCategoryService;
-	
+
 	@GetMapping(value = "admin/category")
-	public ModelAndView index(@ModelAttribute("model") CategoryDTO model)
-	{
+	public ModelAndView index(@ModelAttribute("model") CategoryDTO model) {
 		ModelAndView mav = new ModelAndView("admin/category/index");
 		model.setListResult(iCategoryService.findAll());
-		mav.addObject("cate", iCategoryService.findByID((long)1));
-		mav.addObject("model",model);
+		mav.addObject("cate", iCategoryService.findByID((long) 1));
+		mav.addObject("model", model);
 		return mav;
 	}
+
 	@GetMapping(value = "/admin/category/create")
 	public ModelAndView create(@ModelAttribute("model") CategoryDTO categoryDTO) {
 		ModelAndView mav = new ModelAndView("admin/category/createCategoryProduct");
 		return mav;
 	}
+
 	@PostMapping(value = "/admin/category/create")
 	public String createCate(@ModelAttribute("model") CategoryDTO categoryDTO) {
-		
+
 		iCategoryService.save(categoryDTO);
 		return "admin/category";
 	}
+
 	@GetMapping(value = "/admin/category/edit")
 	public ModelAndView editCate(@RequestParam(value = "id", required = false) Long id) {
 		ModelAndView mav = new ModelAndView("admin/category/editCategoryProduct");
@@ -51,23 +56,24 @@ public class CategoryController {
 		mav.addObject("model", model);
 		return mav;
 	}
+
 	@PostMapping(value = "/admin/category/edit")
-	public String editCate(@ModelAttribute("model") CategoryDTO model) {
-		
+	public RedirectView editCate(@ModelAttribute("model") CategoryDTO model) {
+
 		iCategoryService.save(model);
 
-		ModelAndView mav = new ModelAndView("admin/category/index");
-		model.setListResult(iCategoryService.findAll());
-		mav.addObject("model",model);
-		return "redirect:admin/category";
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl("/bugshop/admin/category");
+		return redirectView;
 	}
-	@GetMapping(value = "/admin/category/delete")
-	public String deleteCate(@RequestParam(value = "id", required = false) Long id) {
-		
-		iCategoryService.delete(id);
 
-		return "redirect:admin/category";
+	@GetMapping(value = "/admin/category/delete")
+	public RedirectView deleteCate(@RequestParam(value = "id", required = false) Long id) {
+
+		iCategoryService.delete(id);
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl("/bugshop/admin/category");
+		return redirectView;
 	}
-	
-	
+
 }
