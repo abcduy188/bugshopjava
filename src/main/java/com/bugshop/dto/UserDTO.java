909c36577ab25d28e7fa0c54ab642d06812af78d
abcduy <tr.khanhsduy@gmail.com>
2022-03-10 23:22:35 +0700

    read user

--- src/main/java/com/bugshop/dto/UserDTO.java
+++ src/main/java/com/bugshop/dto/UserDTO.java
@@ -1,10 +1,31 @@
 package com.bugshop.dto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class UserDTO extends AbstractDTO<UserDTO>{
 
 	private String email;
 	private String password;
 	private String name;
+	private String roleString;
+	private List<String> rolelist = new ArrayList<>();
+	
+	public List<String> getRolelist() {
+		return rolelist;
+	}
+	public void setRolelist(List<String> rolelist) {
+		this.rolelist = rolelist;
+	}
+	public String getRoleString() {
+		return roleString;
+	}
+	public void setRoleString(String roleString) {
+		this.roleString = roleString;
+	}
+	private Set<String> roles = new HashSet<>();
 	public String getEmail() {
 		return email;
 	}
@@ -13,6 +34,12 @@ public class UserDTO extends AbstractDTO<UserDTO>{
 	}
 	public String getPassword() {
 		return password;
+	}
+	public Set<String> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<String> roles) {
+		this.roles = roles;
 	}
 	public void setPassword(String password) {
 		this.password = password;
--- src/main/java/com/bugshop/service/IUserService.java
+++ src/main/java/com/bugshop/service/IUserService.java
@@ -1,7 +1,10 @@
 package com.bugshop.service;
 
+import java.util.List;
+
 import com.bugshop.dto.UserDTO;
 
 public interface IUserService {
 	int register(UserDTO userDTO);
+	List<UserDTO> findAll();
 }
